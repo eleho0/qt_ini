6a81c766bd770f657a18041f9eb2df1f503b334e
eleho0 <wsk_hzb@163.com>
2014-12-07 03:56:14 +0800

    add color button

--- mdiy.cpp
+++ mdiy.cpp
@@ -13,6 +13,16 @@ mDiy::mDiy(QWidget *parent) :
     ui->lineEdit_fruit->setText(fav->value("fav/fruit", "orange").toString());
     delete fav;
 
+    ui->button_9527->setStyleSheet("color: blue;"
+            "background-color: yellow;"
+            "selection-color: yellow;"
+            "selection-background-color: blue;");
+
+    hilight_button_color_all(ui->button_1521, 200, 20, 10, 100);
+    hilight_button_activate(ui->button_2822, 210, 110, 100, 150);
+
+    hilight_button_t(ui->button_exit, 200, 50, 30, 188, 1);
+
     connect(ui->pushButton_ok,SIGNAL(clicked()),this, SLOT(save_setting()));
 }
 
@@ -31,5 +41,76 @@ void mDiy::save_setting()
 
     delete fav;
 
+}
+//set all color Acitvate, Inactivate Disable
+
+void mDiy::hilight_button_color_all(QPushButton *btn, int R, int G, int B, int transparent)
+{
+    QPalette palette = btn->palette();
+    QBrush brush(QColor(R, G, B, transparent));
+    brush.setStyle(Qt::SolidPattern);
+    palette.setBrush(QPalette::Active,   QPalette::Button, brush);
+    palette.setBrush(QPalette::Inactive, QPalette::Button, brush);
+    palette.setBrush(QPalette::Disabled, QPalette::Button, brush);
+    btn->setPalette(palette);
+}
+
+void mDiy::hilight_button_t(QPushButton *btn, int R, int G, int B, int transparent, int layer)
+{
+    QPalette palette = btn->palette();
+    QBrush brush(QColor(R, G, B, transparent));
+    brush.setStyle(Qt::SolidPattern);
+    switch(layer){
+        case 1:
+            palette.setBrush(QPalette::Active,   QPalette::Button, brush);
+            break;
+        case 2:
+            palette.setBrush(QPalette::Inactive, QPalette::Button, brush);
+            break;
+        case 3:
+
+            palette.setBrush(QPalette::Disabled, QPalette::Button, brush);
+            break;
+        default:
+
+            break;
+
+
+    }
+
+    btn->setPalette(palette);
+}
+
+
+
+
+void mDiy::hilight_button_activate(QPushButton *btn, int R, int G, int B, int transparent)
+{
+    QPalette palette = btn->palette();
+    QBrush brush(QColor(R, G, B, transparent));
+    brush.setStyle(Qt::SolidPattern);
+    palette.setBrush(QPalette::Active,   QPalette::Button, brush);
+    btn->setPalette(palette);
+}
+
+void mDiy::hilight_button_Inactivate(QPushButton *btn, int R, int G, int B, int transparent)
+{
+    QPalette palette = btn->palette();
+    QBrush brush(QColor(R, G, B, transparent));
+    brush.setStyle(Qt::SolidPattern);
+    palette.setBrush(QPalette::Inactive, QPalette::Button, brush);
+    btn->setPalette(palette);
+}
+
+void mDiy::hilight_button_Disable(QPushButton *btn, int R, int G, int B, int transparent)
+{
+
+    QPalette palette = btn->palette();
+    QBrush brush(QColor(R, G, B, transparent));
+    brush.setStyle(Qt::SolidPattern);
+    palette.setBrush(QPalette::Disabled, QPalette::Button, brush);
+    btn->setPalette(palette);
 
 }
+
+
--- mdiy.h
+++ mdiy.h
@@ -1,5 +1,6 @@
 #ifndef MDIY_H
 #define MDIY_H
+#include <QPushButton>
 
 #include <QWidget>
 
@@ -19,6 +20,15 @@ private slots:
         void save_setting();
 private:
     Ui::mDiy *ui;
+    //set all color Acitvate, Inactivate Disable
+    void hilight_button_color_all(QPushButton *btn, int R, int G, int B, int transparent);
+
+    void hilight_button_t(QPushButton* btn, int R, int G, int B, int transparent, int layer);
+    void hilight_button_activate(QPushButton *btn, int R, int G, int B, int transparent);
+    void hilight_button_Inactivate(QPushButton *btn, int R, int G, int B, int transparent);
+    void hilight_button_Disable(QPushButton *btn, int R, int G, int B, int transparent);
+    //void hilight_button_color(QPushButton *btn, char* color);
+
 };
 
 #endif // MDIY_H
